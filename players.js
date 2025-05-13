3627d15eefa1a70e48cdf956efc28de793deada2
baileyadler <526BAdler@frhsd.com>
2025-05-13 09:30:26 -0400

    first commit

---
+++ players.js
@@ -0,0 +1,37 @@
+const players = [ 
+    {
+        firstName: 'Sonya',
+        lastName: 'Hoffmann',
+        position: 'Mid',
+        age: '16',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Sonya',
+        lastName: 'Hoffmann',
+        position: 'Mid',
+        age: '16',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Sonya',
+        lastName: 'Hoffmann',
+        position: 'Mid',
+        age: '16',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Sonya',
+        lastName: 'Hoffmann',
+        position: 'Mid',
+        age: '16',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Sonya',
+        lastName: 'Hoffmann',
+        position: 'Mid',
+        age: '16',
+        photo: 'https://picsum.photos/200/300'
+    },
+]
