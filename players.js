82ca352a614c69828906aca78aa8e84b88f98d6c
baileyadler <526BAdler@frhsd.com>
2025-05-16 09:22:34 -0400

    added a nice button

--- players.js
+++ players.js
@@ -34,4 +34,32 @@ const players = [
         age: '16',
         photo: 'https://picsum.photos/200/300'
     },
+    {
+        firstName: 'Sonya',
+        lastName: 'Hoffmann',
+        position: 'Mid',
+        age: '16',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Sonya',
+        lastName: 'Hoffmann',
+        position: 'Mid',
+        age: '16',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Sonya',
+        lastName: 'Hoffmann',
+        position: 'Mid',
+        age: '16',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Sonya',
+        lastName: 'Hoffmann',
+        position: 'Mid',
+        age: '16',
+        photo: 'https://picsum.photos/200/300'
+    },
 ]
